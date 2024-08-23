7e265259a22f7f07acda76e9250ac4a9aab8ef33
Sambit Senapati <sambaapu@gmail.com>
2024-08-23 17:57:48 -0400

    FIFO  implementation in VHDL

--- AAC2M2H2/AAC2M2H2.vhd
+++ AAC2M2H2/AAC2M2H2.vhd
@@ -18,7 +18,28 @@ architecture RTL of FIFO8x9 is
 	type fifo_array is array(7 downto 0) of std_logic_vector(8 downto 0);  -- makes use of VHDL’s enumerated type
 	signal fifo:  fifo_array;
 	signal wrptr, rdptr: unsigned(2 downto 0);
-	signal en: std_logic_vector(7 downto 0);
+	--signal en: std_logic_vector(7 downto 0);
 	signal dmuxout: std_logic_vector(8 downto 0);
-
 begin
+wrptr <= "000" when rising_edge(clk) and WrPtrClr ='1'
+else
+wrptr + 1 when rising_edge(clk) and WrInc = '1'
+else
+wrptr;
+
+rdptr <= "000" when rising_edge(clk) and RdPtrClr = '1'
+else
+rdptr + 1 when rising_edge(clk) and RdInc = '1'
+else
+rdptr;
+
+DataOut <= dmuxout when rden = '1'
+else
+"ZZZZZZZZZ" when rden = '0';
+
+fifo(to_integer(wrptr)) <= DataIn when rising_edge(clk) and wren = '1'
+else
+fifo(to_integer(wrptr));
+
+dmuxout <= fifo(to_integer(rdptr));
+end architecture RTL;
